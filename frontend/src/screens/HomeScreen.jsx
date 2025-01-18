f0eebc2c2131cd59b4640eb4511284fadb135c65
TAYYAB35 <tayyabfmtech@gmail.com>
2025-01-18 14:42:55 +0500

    store and api configure

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import Product from '../components/Product.jsx';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productApiSlice.js'
 
 const HomeScreen = () => {
 
-    const [products, setProducts] = useState();
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get('/api/products');
-            setProducts(data);
-        }
-
-        fetchProduct()
-    }, [])
+    const { data: products, isLoading , error } = useGetProductsQuery();
 
     return (
         <div>
-            <h1>Latest Products</h1>
-            <Row>
-                {products && products.map((product) => (
-                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                        <Product product={product} ></Product>
-                    </Col>
-                ))}
-            </Row>
+            {isLoading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <div>{ error.error}</div>
+            ) : (
+                <div>
+                    <h1>Latest Products</h1>
+                    <Row>
+                        {products && products.map((product) => (
+                            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                                <Product product={product} ></Product>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+            )}
+
         </div>
     )
 }
---
+++ frontend/src/slices/productApiSlice.js
@@ -0,0 +1,24 @@
+import { PRODUCT_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        // Use ** query ** for fetching data(read- only).
+        // Use ** mutation ** for modifying data(write operations).
+        getProducts: builder.query({
+            query: () => ({
+                url: PRODUCT_URL,
+            }),
+            keepUnusedDataFor: 5 // Retain unused data for 5 seconds before removing it from the cache
+        }),
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCT_URL}/${productId}`
+            }),
+            keepUnusedDataFor: 5
+        })
+
+    })
+});
+
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productApiSlice
